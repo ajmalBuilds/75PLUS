5f27abb978210a4ee1fbc61cebeceaa2d4bd9ea7
[Mohammed Ajmal Khan] <[ajmalbuilds@gmail.com]>
2025-03-10 00:25:45 +0530

    Fix: Fix the routes attempt 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Dashboard from "./pages/Dashboard";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LandingPage from './pages/LandingPage';
+import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<LandingPage />} />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
