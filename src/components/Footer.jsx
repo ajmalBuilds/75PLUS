f1a635aa029505da37d29190001db2736c52adde
[Mohammed Ajmal Khan] <[ajmalbuilds@gmail.com]>
2025-03-26 00:29:01 +0530

    FIX: Fix Font in Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="w-screen h-[300px] text-center flex flex-col items-center justify-between mt-[100px]">
-      <h1 style={{ fontFamily: 'ananda, sans-serif'}} className="font-medium text-[30px] mt-5">
+    <footer className="w-ful h-[300px] text-center flex flex-col items-center justify-between mt-[100px]">
+      <h1 style={{ fontFamily: 'ananda, sans-serif'}} className="text-[24px] md:text-[30px] mt-5">
         Mohammed Ajmal Khan
       </h1>
 
       <div className="w-[90%] h-[100px] mt-5 flex items-center justify-center">
-        <ul className="flex justify-center items-center flex-wrap-reverse gap-5">
-          {['Home', 'About', 'Projects', 'Blog', 'Portfolio', 'Contact', 'Services', 'Support'].map((link) => (
+        <ul className="flex justify-center items-center flex-wrap gap-5">
+          {['Home', 'About', 'Blog', 'Portfolio', 'Contact'].map((link) => (
             <li key={link} className="list-none font-semibold text-[18px] font-p">
               <a href="/home" className="no-underline text-black hover:text-gray-700">
                 {link}
@@ -19,7 +19,7 @@ const Footer = () => {
         </ul>
       </div>
 
-      <div style={{ fontFamily: 'poppins, sans-serif'}} className="w-1/2 h-[50px] flex justify-between items-center">
+      {/* <div style={{ fontFamily: 'poppins, sans-serif'}} className="w-1/2 h-[50px] flex justify-between items-center">
         {[
           { icon: 'fa-twitter', link: '#' },
           { icon: 'fa-instagram', link: '#' },
@@ -35,7 +35,7 @@ const Footer = () => {
             <i className={`fa-brands ${plugin.icon}`}></i>
           </a>
         ))}
-      </div>
+      </div> */}
 
       <div style={{ fontFamily: 'poppins, sans-serif'}} className="flex gap-4 mb-6 bg-opacity-50">
         <p className="text-sm font-light">© 2024 Crad Craft</p>
